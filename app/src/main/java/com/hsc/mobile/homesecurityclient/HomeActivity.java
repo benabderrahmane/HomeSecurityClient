b3cd244d89894ba782c0888a7c2c30fc74b3c906
Benabderrahmane <benabderrahmane.yacine@gmail.com>
2017-05-24 11:08:00 +0200

    Initial commit

---
+++ app/src/main/java/com/hsc/mobile/homesecurityclient/HomeActivity.java
@@ -0,0 +1,38 @@
+package com.hsc.mobile.homesecurityclient;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private Button home_login;
+    private Button home_signin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        home_login = (Button) findViewById(R.id.home_login);
+        home_signin = (Button) findViewById(R.id.home_signin);
+
+        //Définition de l'evenement lors du click sur les bouttons
+        home_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        home_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), FirstUserActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
